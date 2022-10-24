3d497e03b1df5884cdcf390ced9624ed7187a625
GuiceJoose <snowmanjz@gmail.com>
2022-10-24 12:10:48 -0500

    set number of notifications to 0 when all are marked as read

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const markAllReadButton = document.querySelector(".mark-all-as-read");
+const notificationsTotal = document.querySelector(".notifications-total");
 
 function handleMarkAllRead() {
   const notifications = document.querySelectorAll("li");
   notifications.forEach((notification) => {
     notification.classList.remove("unread");
   });
+  notificationsTotal.textContent = "0";
 }
 markAllReadButton.addEventListener("click", handleMarkAllRead);
